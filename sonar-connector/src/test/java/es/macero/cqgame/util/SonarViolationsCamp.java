4ffd5859f62d020220f508fac8d6605c015e503e
Moisés Macero <mechero@users.noreply.github.com>
2015-12-20 12:03:12 +0100

    Including Sonar violations on purpose

--- sonar-connector/src/test/java/es/macero/cqgame/util/SonarViolationsCamp.java
+++ sonar-connector/src/test/java/es/macero/cqgame/util/SonarViolationsCamp.java
@@ -9,6 +9,7 @@ public class SonarViolationsCamp {
         String s = "This is a message";
         if(s == "This is a"){
             System.out.println("Miracle");
+            throw new NullPointerException();
         }
     }
 }
