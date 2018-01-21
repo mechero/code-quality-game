8cf90df799a079004e3cfe6907588557c1091b1f
mechero <mechero@users.noreply.github.com>
2018-01-21 10:06:12 +0100

    Upgraded to newer versions, fixes, updated README file.

--- frontend/app/main.js
+++ frontend/app/main.js
@@ -1,6 +1,7 @@
 "use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app.module');
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
+Object.defineProperty(exports, "__esModule", { value: true });
+const platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+const app_module_1 = require("./app.module");
+const platform = platform_browser_dynamic_1.platformBrowserDynamic();
 platform.bootstrapModule(app_module_1.AppModule);
 //# sourceMappingURL=main.js.map
