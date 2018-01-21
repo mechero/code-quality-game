deb81600a154714ac4d83399dd872599b3e5cc4f
mechero <mechero@users.noreply.github.com>
2018-01-21 15:28:27 +0100

    Fix issues (1)

--- sonar-connector/src/main/java/es/macero/cqgame/modules/configuration/dao/SonarServerConfigurationDaoImpl.java
+++ sonar-connector/src/main/java/es/macero/cqgame/modules/configuration/dao/SonarServerConfigurationDaoImpl.java
@@ -7,21 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Properties;
-
 @Component
 public class SonarServerConfigurationDaoImpl implements SonarServerConfigurationDao {
 
     private static final Log log = LogFactory.getLog(SonarServerConfigurationDaoImpl.class);
-
-    private static final String URL_PROPERTY = "url";
-    private static final String USER_PROPERTY = "user";
-    private static final String PASSWORD_PROPERTY = "password";
-    private static final String PROPERTIES_FILE_NAME = "sonar-server.properties";
 
     private SonarServerConfiguration sonarServerConfiguration;
 
@@ -36,44 +25,12 @@ public class SonarServerConfigurationDaoImpl implements SonarServerConfiguration
         this.defaultUrl = sonarUrl;
         this.defaultUser = sonarUser;
         this.defaultPassword = sonarPassword;
-        this.sonarServerConfiguration = readConfigurationFromFile(PROPERTIES_FILE_NAME);
+        this.sonarServerConfiguration = createSonarServerConfiguration();
     }
 
-    private SonarServerConfiguration readConfigurationFromFile(final String propertiesFileName) {
-        final Properties properties = new Properties();
-        try {
-            final File file = new File(propertiesFileName);
-            log.info("Loading Sonar server configuration from: " + file.getAbsolutePath());
-            if (file.exists()) {
-                properties.load(new FileReader(file));
-                return new SonarServerConfiguration(properties.getProperty(URL_PROPERTY),
-                        properties.getProperty(USER_PROPERTY), properties.getProperty(PASSWORD_PROPERTY));
-            } else {
-                log.warn("Configuration file can't be found, using application properties file.");
-                return new SonarServerConfiguration(defaultUrl, defaultUser, defaultPassword);
-            }
-        } catch (final Exception e) {
-            log.error("Error loading configuration file. Loading defaults from application properties file", e);
-            return new SonarServerConfiguration(defaultUrl, defaultUser, defaultPassword);
-        }
-    }
-
-    @Override
-    public boolean saveConfiguration(final SonarServerConfiguration configuration) {
-        final Properties properties = new Properties();
-        properties.setProperty(URL_PROPERTY, configuration.getUrl());
-        properties.setProperty(USER_PROPERTY, configuration.getUser());
-        properties.setProperty(PASSWORD_PROPERTY, configuration.getPassword());
-        try {
-            final File file = new File(PROPERTIES_FILE_NAME);
-            log.info("Storing properties into " + file.getAbsolutePath());
-            properties.store(new FileWriter(file), "Sonar server properties");
-            sonarServerConfiguration = configuration;
-        } catch (final IOException e) {
-            log.error("Error saving configuration", e);
-            return false;
-        }
-        return true;
+    private SonarServerConfiguration createSonarServerConfiguration() {
+        log.warn("Configuration file can't be found, using application properties file.");
+        return new SonarServerConfiguration(defaultUrl, defaultUser, defaultPassword);
     }
 
     @Override
