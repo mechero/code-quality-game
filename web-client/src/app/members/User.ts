b7a6055a2525aca110cc4e31fddb5f51c72ddb3b
mechero <mechero@users.noreply.github.com>
2018-10-17 07:27:37 +0200

    Fixed bug: unassigned users should not keep the team name

--- web-client/src/app/members/User.ts
+++ web-client/src/app/members/User.ts
@@ -1,4 +1,12 @@
 export class User {
+
+  constructor(id: string, login: string, alias: string, team: string) {
+    this.id = id;
+    this.login = login;
+    this.alias = alias;
+    this.team = team;
+  }
+
   id: string;
   login: string;
   alias: string;
